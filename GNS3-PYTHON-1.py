0a9c2de85721271bb8ac79ad37c51451da39ed9b
defxsec <defxsec@gmail.com>
2022-07-29 10:25:21 -0400

    Primir commit

---
+++ GNS3-PYTHON-1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+import getpass
+import telnetlib
+
+HOST = "192.168.1.10"
+user = input("Enter your telnet username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"ls\n")
+
+tn.write(b"enable\n")
+tn.write(b"cisco\n")
+tn.write(b"configure terminal\n")
+tn.write(b"interface loopback 0\n")
+tn.write(b"ip address 1.1.1.1 255.255.255.255\n")
+tn.write(b"end\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
---
+++ GNS3-PYTHON-4.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+from ast import For
+import getpass
+import telnetlib
+
+HOST = "192.168.1.11"
+user = input("Enter your telnet username: ")
+password = getpass.getpass()
+
+tn = telnetlib.Telnet(HOST)
+
+tn.read_until(b"Username: ")
+tn.write(user.encode('ascii') + b"\n")
+if password:
+    tn.read_until(b"Password: ")
+    tn.write(password.encode('ascii') + b"\n")
+
+tn.write(b"ls\n")
+
+tn.write(b"configure terminal\n")
+
+for n in range(2, 10):
+    tn.write(b"vlan " + str(n).encode('ascii') + b"\n")
+    tn.write(b"name Python_VLAN_" + str(n).encode('ascii') + b"\n")
+
+tn.write(b"end\n")
+tn.write(b"exit\n")
+
+print(tn.read_all().decode('ascii'))
